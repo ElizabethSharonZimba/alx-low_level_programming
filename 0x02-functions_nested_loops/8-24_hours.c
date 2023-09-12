50c3e381319f54d0c0eb6a8805b408c84cca17fe
ElizabethSaronZimba <sharonzimba95@gmail.com>
2023-09-12 15:36:45 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "mai.h"
+/**
+ * jack_bauer - returns every minute of the day of jack Bauer
+ * from 00:00 to 25:59
+ */
+void jack_bauer(void)
+{
+int i, j;
+i = 0;
+while (i < 24}
+{
+j = 0;
+while (j < 60)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+_putchar((':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+_putchar('\n');
+j++;
+}
+i++;
+}
+}
