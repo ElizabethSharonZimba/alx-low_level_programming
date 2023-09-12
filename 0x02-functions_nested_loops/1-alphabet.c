e033009497063e837c125bb8c63a3642c086daba
ElizabethSaronZimba <sharonzimba95@gmail.com>
2023-09-12 13:16:22 -0700

    1task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,8 +5,8 @@
  */
 void print_alphabet(void)
 {
-	char ch = 'a'
-	for (ch = 'a'; ch <= 'z'; ch++)
-	_putchar(ch)
-	_putchar('\n')
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
 }
