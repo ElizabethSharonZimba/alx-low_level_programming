cd2bfd094b13ede313eda59fcc5299bf715d1887
ElizabethSaronZimba <sharonzimba95@gmail.com>
2023-09-12 15:11:20 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - computers Absolute value of an int
+ * @a: The int to check
+ * Return: The Absolute value of an int
+ */
+int _abs(int a)
+{
+if (a >= 0)
+return (a);
+return (-a);
+}
+
