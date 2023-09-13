3174a24506461e8cf549da4a225ffe78de530a55
ElizabethSaronZimba <sharonzimba95@gmail.com>
2023-09-13 11:44:56 -0700

    main c 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - checks if int is + or -
+ * Return: (0) successful execution
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
