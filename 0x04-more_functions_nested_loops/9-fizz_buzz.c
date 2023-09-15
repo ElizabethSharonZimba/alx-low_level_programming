a5ac16214c2b0ee1bca6ea15ad856ffe8ef00bd5
ElizabethSaronZimba <sharonzimba95@gmail.com>
2023-09-15 10:12:33 -0700

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 /**
- * main - prints "fizz" for digits that be divided by 3
- * prints "Buzz" for digits that can be divide by 5
- * prints "FizzBuzz" for digits that can be divided by 3, on digits thaat are from 1-100
- * Return: 0 sucessful execution
+ * main - prints "fizz" for digits  divided by 3
+ * prints "Buzz" for digit divided by 5
+ * prints "FizzBuzz" for digit divided by 3,on digits thaat are from 1-100
+ * Return: 0 sucess
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 99; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%i  ", i);
+	}
+	printf("Buzz\n");
+	return (0);
+}
