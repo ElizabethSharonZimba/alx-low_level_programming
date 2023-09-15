dc839300b4e21340ad2f48e120a5a93264534579
ElizabethSaronZimba <sharonzimba95@gmail.com>
2023-09-15 09:16:34 -0700

    s

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * print_triangle - prin the leght of the triangle
  * @size: triangle height
@@ -8,9 +9,11 @@ void print_triangle(int size);
 	int i, m;
 
 	m = size;
+	{
 
 	if (size <= 0)
 		_putchar('\n');
+	}
 	while (size > 0)
 	{
 		for (i = 0; i < m; i++)
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/**
+ * main - prints "fizz" for digits that be divided by 3
+ * prints "Buzz" for digits that can be divide by 5
+ * prints "FizzBuzz" for digits that can be divided by 3, on digits thaat are from 1-100
+ * Return: 0 sucessful execution
