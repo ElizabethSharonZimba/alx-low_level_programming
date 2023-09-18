747f3da7403e502ec8feb0e1ead67d8a382a0b99
ElizabethSaronZimba <sharonzimba95@gmail.com>
2023-09-18 10:09:42 -0700

    11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - password random generator
+ * Return: 0 succeesful execution
+ */
+int main(void)
+{
+	int sum = o;
+	char c;
+
+	srand(time(NULL));
+	while (sum < 2772)
+	{
+		c = rand() % 94 + 33;
+		putchar(c);
+		sum += c;
+	}
+	putchar(2772 - sum);
+	return (0);
+}
